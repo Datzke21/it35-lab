c5a281c253ba62522d8e51de4568ec661ff0a95b
Daryl <Daryldada697@gmail.com>
2025-02-17 10:58:04 +0800

    add package jason changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,35 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, 
+  IonHeader, 
+  IonIcon,
+   IonLabel,
+    IonPage, 
+    IonRouterOutlet, 
+    IonTabBar, 
+    IonTabButton, 
+    IonTabs, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home: React.FC = () => {  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Home page</IonTitle>
+          <IonTitle>Daryl Dajao</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <center>App Dev</center>
+            
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
-      </IonContent>
+      </IonContent >
     </IonPage>
   );
 };
