8595526ffaffaf92df266c1450430523bb8ff716
Daryl <Daryldada697@gmail.com>
2025-02-18 15:48:01 +0800

    Updates

--- assets/keyboard2-legacy-DHWYHKNZ.js
+++ assets/keyboard2-legacy-DHWYHKNZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DtXdFC-A.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-B1uGPD4K.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
