66cd0034bb900224a7d9e6ed969f3ef6c2cec1fa
Daryl <Daryldada697@gmail.com>
2025-03-07 11:17:36 +0800

    Updates

--- assets/index3-legacy-BSKznvjr.js
+++ assets/index3-legacy-BSKznvjr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B6b2fjeS.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-CAGsGFrg.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-Dyqt7gZl.js
+++ assets/md.transition-legacy-Dyqt7gZl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B6b2fjeS.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-CAGsGFrg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
