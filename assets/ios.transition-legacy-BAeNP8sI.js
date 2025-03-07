0a38e373f2e0879cdf26437547c141fb76bc2358
Daryl <Daryldada697@gmail.com>
2025-03-07 16:52:41 +0800

    Updates

--- assets/ios.transition-legacy-BAeNP8sI.js
+++ assets/ios.transition-legacy-BAeNP8sI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CAGsGFrg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BT8_oBfD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BJ2MVxK-.js
+++ assets/swipe-back-legacy-BJ2MVxK-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CAGsGFrg.js","./index3-legacy-BSKznvjr.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BT8_oBfD.js","./index3-legacy-CPUWiED_.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
