18f20eec402caa398a4d9e1e62b55790384eb1f9
Daryl <Daryldada697@gmail.com>
2025-03-08 19:15:18 +0800

    Added Avatar update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,6 +25,12 @@ import {
     const doLogin = () => {
         navigation.push('/it35-lab/app','forward','replace');
     }
+    function setEmail(arg0: string): void{
+      throw new Error('Function not implemented.');
+    }
+    function setPassword(args0: string): void{
+      throw new Error('Function not implemented');
+    }
     return (
       <IonPage>
         <IonHeader>
@@ -36,23 +42,35 @@ import {
           </IonToolbar> 
         </IonHeader>
 
-        <IonAvatar>
-        <img alt="Sample" src="https://tse1.mm.bing.net/th?id=OIP.rX298mOw30d82kcPsL9cxgHaEK&pid=Api&P=0&h=180qzytdg8xliuaelun.jpg"></img>
-        </IonAvatar>           
-        <IonList>
-      <IonItem>
-        <IonInput labelPlacement="stacked" label="" placeholder="Email">
-          <IonIcon slot="start" icon={lockClosed} aria-hidden="true"></IonIcon>
-          <IonButton fill="clear" slot="end" aria-label="Show/hide">
-            <IonIcon slot="icon-only" name={eye} aria-hidden="true"></IonIcon>
-          </IonButton>
-        </IonInput>
-        <IonInput placeholder="Password" label="">
-          <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-        </IonInput>
-      </IonItem>
-    </IonList>
+      
+        
         <IonContent className='ion-padding'>
+        <div style={{textAlign: "center", marginBottom: "20px"}}>
+          <IonAvatar style={{width: "120px", heigh: "120px", margin: "auto"}}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.rX298mOw30d82kcPsL9cxgHaEK&pid=Api&P=0&h=180qzytdg8xliuaeLun.jpg' alt='avatar'
+            style={{width: "100%", height: "100px", borderRadius: "50%"}}
+            />
+          </IonAvatar>
+        </div>
+        <>
+        <IonInput
+        label='Email'
+        labelPlacement="floating" 
+          value=""
+          onIonInput={(e) => setEmail(e.detail.value!)}
+          placeholder="Enter email" />
+
+          < br/>
+          <IonInput 
+          type="password" 
+          label="Password" 
+          labelPlacement="floating"
+          value=""
+          onIonInput={(e) => setPassword(e.detail.value!)}
+          placeholder="Enter password" />
+            <IonInputPasswordToggle slot="end" />
+            
+        </>
             <IonButton onClick={() => doLogin()} expand='full'>
                 login
             </IonButton>
