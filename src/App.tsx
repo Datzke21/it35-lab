f6f843f9604acc20407ca5506c5ee27abab7efdb
Daryl <Daryldada697@gmail.com>
2025-03-15 19:35:06 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,10 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Register from './pages/Register';
+import Signup from './pages/Signup';
+
+
 
 setupIonicReact();
 
@@ -41,6 +45,8 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route exact path="/it35-lab" component={Login} />
         <Route path="/it35-lab/app" component={Menu} />
+        <Route path="/it35-lab/register" component={Register}/>
+        <Route path="/it35-lab/Signup" component={Signup} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
