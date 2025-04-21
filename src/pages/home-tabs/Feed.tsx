7059351ebe893889037dc15392fa7405a9b95042
Daryl <Daryldada697@gmail.com>
2025-04-21 10:00:03 +0800

    Feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,65 +1,40 @@
 import { 
-  IonAvatar,
-  IonButton,
-    IonButtons,
-    IonIcon,
-    IonItem,
-    IonText,
-    IonList,
-      IonCard,
-      IonCardContent,
-      IonCardSubtitle,
-      IonCardTitle,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      IonLabel
-  } from '@ionic/react';
-  import { chatbubbleEllipsesOutline, ellipsisHorizontal, heart, paperPlaneOutline } from 'ionicons/icons';
-  const Feed: React.FC = () => {
-    return (
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <IonList lines='none'>
-          <IonItem className='ion-margin-top'>
-            <IonAvatar slot='start'>
-            <img alt="Sample" src="https://tse1.mm.bing.net/th?id=OIP.rX298mOw30d82kcPsL9cxgHaEK&pid=Api&P=0&h=180qzytdg8xliuaelun.jpg"/>
-            </IonAvatar>
-            <IonLabel>
-              <IonText>
-                One piece
-              </IonText>
-            </IonLabel>
-          </IonItem>
-        </IonList>
-          <IonCard
->         <IonHeader>
-          <IonCardTitle>One Piece</IonCardTitle>
-          <IonCardSubtitle>Zoro</IonCardSubtitle>
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
       </IonHeader>
-      
-      <IonCardContent>"Only those who have suffered long, can see the light within the shadows." – Roronoa Zoro
+      <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
+        <FeedContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
 
-        <IonButton fill='clear'>Action1</IonButton>
-        
-        <IonButton fill='clear'>Action2</IonButton>
-
-      </IonCardContent>
-  </IonCard>        
-  </IonContent>
-      </IonPage>
-    );
-  };
-  export default Feed;
+export default Feed;
